12eaf2722721b0d30143c63bba71e80b09289430
Syah Noorazman <syah-noorazman_ishak@astro.com.my>
2025-05-26 17:46:36 +0800

    Update openai_usage_tracker

--- src/Controller/UsageDashboardController.php
+++ src/Controller/UsageDashboardController.php
@@ -6,18 +6,23 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Database\Connection;
 use Drupal\user\Entity\User;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Drupal\openai_usage_tracker\Form\FilterForm;
 
 class UsageDashboardController extends ControllerBase {
 
   protected $database;
+  protected $request;
 
-  public function __construct(Connection $database) {
+  public function __construct(Connection $database, RequestStack $request_stack) {
     $this->database = $database;
+    $this->request = $request_stack->getCurrentRequest();
   }
 
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('database')
+      $container->get('database'),
+      $container->get('request_stack')
     );
   }
 
@@ -25,10 +30,24 @@ class UsageDashboardController extends ControllerBase {
     $query = $this->database->select('openai_usage_log', 'log')
       ->fields('log')
       ->orderBy('created', 'DESC')
-      ->range(0, 50);
-    $results = $query->execute()->fetchAll();
+      ->range(0, 100);
 
+    // Get filters from URL
+    $filters = [
+      'user_id' => $this->request->query->get('user_id'),
+      'module' => $this->request->query->get('module'),
+      'model' => $this->request->query->get('model'),
+    ];
+
+    foreach ($filters as $field => $value) {
+      if (!empty($value)) {
+        $query->condition("log.$field", $value);
+      }
+    }
+
+    $results = $query->execute()->fetchAll();
     $rows = [];
+
     foreach ($results as $row) {
       $user = User::load($row->user_id);
       $rows[] = [
@@ -51,10 +70,15 @@ class UsageDashboardController extends ControllerBase {
     ];
 
     return [
-      '#type' => 'table',
-      '#header' => $header,
-      '#rows' => $rows,
-      '#empty' => $this->t('No usage data found.'),
+      'form' => $this->formBuilder()->getForm(FilterForm::class),
+      'table' => [
+        '#type' => 'table',
+        '#header' => $header,
+        '#rows' => $rows,
+        '#empty' => $this->t('No usage data found.'),
+        '#attributes' => ['class' => ['usage-log-table']],
+      ],
     ];
   }
+
 }
--- src/Form/FilterForm.php
+++ src/Form/FilterForm.php
@@ -1,1 +1,100 @@
 <?php
+
+namespace Drupal\openai_usage_tracker\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\user\Entity\User;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class FilterForm extends FormBase {
+
+  protected $database;
+
+  public function __construct(Connection $database) {
+    $this->database = $database;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database')
+    );
+  }
+
+  public function getFormId() {
+    return 'openai_usage_filter_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Fetch unique modules & models
+    $modules = $this->getDistinctValues('module');
+    $models = $this->getDistinctValues('model');
+
+    // User list
+    $users = ['' => '- Any -'];
+    $user_query = $this->database->select('openai_usage_log', 'l')
+      ->fields('l', ['user_id'])
+      ->distinct()
+      ->execute();
+    foreach ($user_query as $row) {
+      $account = User::load($row->user_id);
+      if ($account) {
+        $users[$row->user_id] = $account->getDisplayName();
+      }
+    }
+
+    // Filter Fields
+    $form['user_id'] = [
+      '#type' => 'select',
+      '#title' => $this->t('User'),
+      '#options' => $users,
+      '#default_value' => \Drupal::request()->query->get('user_id'),
+    ];
+
+    $form['module'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Module'),
+      '#options' => ['' => '- Any -'] + array_combine($modules, $modules),
+      '#default_value' => \Drupal::request()->query->get('module'),
+    ];
+
+    $form['model'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Model'),
+      '#options' => ['' => '- Any -'] + array_combine($models, $models),
+      '#default_value' => \Drupal::request()->query->get('model'),
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Filter'),
+    ];
+
+    return $form;
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $params = [];
+    foreach (['user_id', 'module', 'model'] as $field) {
+      $val = $form_state->getValue($field);
+      if (!empty($val)) {
+        $params[$field] = $val;
+      }
+    }
+
+    $form_state->setRedirect('openai_usage_tracker.dashboard', [], ['query' => $params]);
+  }
+
+  private function getDistinctValues($field) {
+    $query = $this->database->select('openai_usage_log', 'l')
+      ->fields('l', [$field])
+      ->distinct()
+      ->execute();
+    return array_column(iterator_to_array($query), $field);
+  }
+
+}
